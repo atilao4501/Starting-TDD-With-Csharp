db1f59a715157d7bee6c00fe783938ada65d9a91
atila.alcantara <atila.alcantara@GNA-N217>
2023-09-20 16:44:53 -0300

    Adicionar arquivos de projeto.

---
+++ Agenda.DAL/Contatos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data.SqlClient;
+using Agenda.Domain;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Drawing;
+using Dapper;
+using System.Linq;
+
+namespace Agenda.DAL
+{
+    public class Contatos
+    {
+        string _strCon;
+
+        public Contatos() {
+
+            IConfigurationRoot configuracao = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            _strCon = configuracao.GetConnectionString("AgendaTest");   
+
+        }
+        
+
+        public void Adicionar(Contato contato)
+        {
+            using(var con = new SqlConnection(_strCon))
+            {
+                con.Execute("insert into Contato (Id, Nome) values (@Id, @Nome)", contato);
+            }
+            
+        }
+
+        public Contato Obter(Guid id) {
+
+            Contato contato;
+
+            using (var con = new SqlConnection(_strCon))
+            {
+                contato = con.QueryFirst<Contato>("select Id, Nome from Contato where Id = @Id", new { Id = id });
+                
+               
+            }
+            return contato;
+        }
+        public List<Contato> ObterTodos()
+        {
+            var contatos = new List<Contato>();
+
+            using (var con = new SqlConnection(_strCon))
+            {
+                contatos = con.Query<Contato>("select Id, Nome from Contato").ToList();           
+
+            }
+            
+            return contatos;
+        }
+
+    }
+    
+}
---
+++ Agenta.DAL.Test/ContatosTest.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using Agenda.DAL;
+using Agenda.Domain;
+
+
+namespace Agenta.DAL.Test
+{
+
+    [TestFixture]
+    public class ContatosTest : BaseTest
+    {
+        Contatos _contatos;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _contatos = new Contatos(); 
+        }
+
+        [Test] 
+        public void IncluirContatoTest()
+        {
+            var contato = new Contato()
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Pedro"
+            };
+          
+            _contatos.Adicionar(contato);
+
+
+           
+            Assert.True(true);
+        }
+
+
+        [Test]
+        public void ObterContatoTest()
+        {
+
+            var contato = new Contato()
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Maria"
+            };
+
+            Contato contatoResultado;
+
+            _contatos.Adicionar(contato);
+
+            contatoResultado = _contatos.Obter(contato.Id);
+
+            Assert.AreEqual(contato.Id, contato.Id);
+            Assert.AreEqual(contato.Nome, contato.Nome);
+        }
+
+        [Test]
+        public void ObterTodosOsContatosTest()
+        {
+            var contato1 = new Contato()
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Maria"
+            };
+            var contato2 = new Contato()
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Joao"
+            };
+
+            _contatos.Adicionar(contato1);
+            _contatos.Adicionar(contato2);
+            var lstContato =  _contatos.ObterTodos();
+            var contatoResultado = lstContato.Where(o => o.Id == contato1.Id).First();
+
+            Assert.IsTrue(lstContato.Count() > 1);
+            Assert.AreEqual(contato1.Id, contatoResultado.Id);
+            Assert.AreEqual(contato1.Nome, contatoResultado.Nome);
+
+        }
+
+        [TearDown]
+        public void TearDown() {
+            _contatos = null;
+        }
+    }
+}
